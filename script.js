8e45e8db449812bd017fe3144ba402efbe5a0a31
“Matthew8991” <matthewmmclanahan@gmail.com>
2022-03-01 10:46:48 -0500

    Created JSBasicsExercise folder and script.js file.

---
+++ script.js
@@ -0,0 +1,47 @@
+console.log(59);
+
+/*
+Strings
+Boolean
+Numbers
+Null
+Undefined
+*/
+
+// Addition is +
+// Suptraction is -
+// Multiplication is *
+// Division is /
+// Modulus is %
+// Exponentiation is **
+
+console.log(5 % 4); // 1
+console.log(5 % 2); // 1
+
+console.log('I\'M MARY POPPINS, Y\'ALL!');
+
+console.log("Hawk" + "eye");
+
+console.log("Matthew".length + "McClanahan".length);
+
+console.log("Avengers: Endgame"[16]);
+
+console.log("\\");
+
+console.log((5 ** 4) - 9);
+
+console.log("I".length + "Am".length % "Iron".length - "Man".length);
+
+console.log("CAP"[1]);
+console.log("VISION"[0]);
+console.log("OKOYE"[4]);
+console.log("NICK FURY"[0]);
+console.log("GROOT"[0]);
+console.log("DOCTOR STRANGE"[13]);
+console.log("THOR"[3]);
+console.log("STAR-LORD"[0]);
+
+let m = 25;
+let c = 4;
+let u = m + c;
+console.log(u)
